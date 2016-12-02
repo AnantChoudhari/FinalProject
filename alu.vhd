b5a09057f0d49b75a77941ad793c12043042b413
amr1006 <amr1006@nyu.edu>
2016-12-01 21:15:29 -0500

    Add files via upload

--- alu.vhd
+++ alu.vhd
@@ -1,74 +1,49 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    19:26:11 11/28/2016 
--- Design Name: 
--- Module Name:    alu - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use ieee.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity alu is
-port(r1: in std_logic_vector(31 downto 0); --Register R1
-     r2: in std_logic_vector(31 downto 0); --Register R2
-	  fn: in std_logic_vector(5 downto 0);  --Function(Hex)
-	  op: in std_logic_vector(5 downto 0);  --Opcode
-	  res: out std_logic_vector(31 downto 0) --Final Output
-	  );
-	  
+port (
+clk, clr : in std_logic
+		);
 
 end alu;
 
 architecture Behavioral of alu is
+signal op1 : std_logic_vector(31 downto 0);
+signal op2 : std_logic_vector(31 downto 0);
+signal ALUop : std_logic_vector(2 downto 0);
+signal output : std_logic_vector(31 downto 0);
+
 begin
-process(r1,r2,fn,op)
-begin
-IF(op = "000000") then
 
-case fn is
-  when "010000" =>   res <= r1 + r2;
-  when "010001" =>   res <= r1 - r2;
-  when "010010" =>   res <= r1 and r2;
-  when "010011" =>   res <= r1 or r2;
-  when "010100" =>   res <= r1 nor r2;
-  when others  =>    res <= "00000000000000000000000000000000";
-  end case;
-
- 
-ELSE
- 
-case op is
- when "000001" => 	res <= r1+r2;
- when "000010" =>    res <= r1-r2;
- when "000011" =>    res <= r1 and r2;
- when "000100" =>    res <= r1 or r2;
- when others   =>    res <= "00000000000000000000000000000000";
- end case;
- end if;  
-end process;
-
+process(clk, clr, ALUop)
+if (clr = '0') then
+	op = x"00000000";
+	elsif (clk'event and clk = '1') then
+	
+		case ALUop is
+			when "001" => output <= op1 + op2;
+			when "010" => output <= op1 - op2;
+			when "011" => output <= op1 and op2;
+			when "100" => output <= op1 or op2;
+			when "101" => output <= op1 nor op2;
+			when "101" => 
+				with op2(4 downto 0) select
+					output <= 	op1(30 downto 0) & '0',
+									op1(29 downto 0) & "00",
+									....
+									-- other cases need to be written
+									
+			when "110" => 
+				with op2(4 downto 0) select
+					output <=	'0' & op1(31 downto 1),
+									"00" & op1(31 downto 2),
+									....
+									-- other cases need to be written
+									
+			when others => output <= x"00000000";
 
 end Behavioral;
 
---
+++ decoder.vhd
@@ -0,0 +1,159 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity decoder is
+port(
+clk, clr : in std_logic
+		);
+
+end decoder;
+
+architecture Behavioral of decoder is
+
+signal op : std_logic_vector(5 downto 0);
+signal func : std_logic_vector(5 downto 0);
+signal isLess : std_logic;)
+
+signal isLoad : std_logic;
+signal isStore : std_logic; 
+signal I_type : std_logic;
+signal isBranch : std_logic;
+signal J_type : std_logic;
+signal WrtEn : std_logic;
+signal ALUop : std_logic_vector(2 downto 0);
+signal NextPC : std_logic_vector(1 downto 0);
+
+begin
+
+op <= inst(31 downto 26);
+func <= inst(5 downto 0);
+
+process(clr,clk,isLoad)
+begin
+	if (clr = '0') then
+		isLoad <= 0;
+	elsif (clk'event and clk = '1') then
+		if (op = "000111") then
+			isLoad <= 1;
+		else isLoad <= 0;
+		end if;
+	end if;
+end process;
+
+process(clr,clk,isStore)
+begin
+	if (clr = '0') then
+		isStore <= 0;
+	elsif (clk'event and clk = '1') then
+		if (op = "001000") then
+			isStore <= 1;
+		else isStore <= 0;
+		end if;
+	end if;
+end process;
+
+process(clr,clk,I_type)
+begin
+	if (clr = '0') then
+		I_type <= 0;
+	elsif (clk'event and clk = '1') then
+		if (op = "000000" or op = "001100" or op = "111111") then
+			I_type <= 0;
+		else I_type <= 1;
+		end if;
+	end if;
+end process;
+
+process(clr,clk,J_type)
+begin
+	if (clr = '0') then
+		J_type <= 0;
+	elsif (clk'event and clk = '1') then
+		if (op = "001100" or op = "111111") then
+			J_type <= 1;
+		else J_type <= 0;
+		end if;
+	end if;
+end process;
+
+process(clr,clk,isBranch)
+begin
+	if (clr = '0') then
+		isBranch <= 0;
+	elsif (clk'event and clk = '1') then
+		if (op = "001001" or op = "001010" or op = "001011") then
+			isBranch <= 1;
+		else isBranch <= 0;
+		end if;
+	end if;
+end process;
+
+process(clr,clk,WrtEn)
+begin
+	if (clr = '0') then
+		WrtEn <= 0;
+	elsif (clk'event and clk = '1') then
+		if (isStore = '1' or isBranch = '1' or J_type = '1') then
+			WrtEn <= 0;
+		else WrtEn <= 1;
+		end if;
+	end if;
+end process;
+
+
+--NOP	000
+--ADD	001
+--SUB	010
+--AND	011
+--OR	100
+--NOR	101	
+--SHL	110
+--SHR	111
+
+
+process(clr,clk,ALUop)
+begin
+if (clr = '0') then
+		ALUop <= "000";
+	elsif (clk'event and clk = '1') then
+
+		case op is
+			when "000000" =>
+				with func select
+				ALUop <=	"001" when "010000",
+						"010" when "010001",
+						"011" when "010010",
+						"100" when "010011",
+						"101" when "010100",
+						"000" when others;
+		
+	when "000001" => ALUop <= "001";
+	when "000010" => ALUop <= "010";
+	when "000011" => ALUop <= "011";
+	when "000100" => ALUop <= "100";
+	when "000101" => ALUop <= "110";
+	when "000110" => ALUop <= "111";
+	when "000111" => ALUop <= "001";
+	when "001000" => ALUop <= "001";
+	when others   => ALUop <= "000";
+end case;
+end if;
+end process;
+
+process(clk,clr,NextPC)
+begin
+if ((op = "001001" and isLess = '1') or (op = "001010" and isEq = '1') or (op = "001011" and isEq = '0')) then
+	NextPC = "10";
+	elsif (J_Type = '1') then
+		NextPC = "01";
+		else
+			NextPC = "00";
+end if;
+
+end process;
+
+end Behavioral;
+
+
